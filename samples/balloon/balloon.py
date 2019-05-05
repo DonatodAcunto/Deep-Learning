20618807b95cb0bc1a945d9f86e4041584b0185a
AndreaMontibeller <45923370+AndreaMontibeller@users.noreply.github.com>
2019-05-05 12:41:48 +0200

    Add files via upload

--- samples/balloon/balloon.py
+++ samples/balloon/balloon.py
@@ -192,11 +192,11 @@ def train(model):
     # Since we're using a very small dataset, and starting from
     # COCO trained weights, we don't need to train too long. Also,
     # no need to train all layers, just the heads should do it.
-    print("Training network heads")
+    print("Training network carpocapsa")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=30,
-                layers='heads')
+                layers='carpocapsa')
 
 
 def color_splash(image, mask):
